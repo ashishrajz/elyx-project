726e9f94674bc3fc733fc89e642b714f820798b7
ashishrajz <ASHISH.RAJ.MEC24@ITBHU.AC.IN>
2025-08-17 21:25:22 +0530

    Delete src/app/api/metrics directory

--- src/app/api/metrics/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import dbConnect from "@/lib/db";
-import Metric from "@/models/Metric";
-
-export async function POST(req: Request) {
-  await dbConnect();
-  const metric = await Metric.create(await req.json());
-  return NextResponse.json(metric);
-}
